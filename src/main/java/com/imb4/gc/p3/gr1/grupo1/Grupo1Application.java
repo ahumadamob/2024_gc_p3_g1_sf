f068a4aa1aa54fed229b52fab287a7a928486b8c
Mario Ahumada <ahumadamob@gmail.com>
2024-03-24 17:39:16 -0300

    First commit

---
+++ src/main/java/com/imb4/gc/p3/gr1/grupo1/Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.imb4.gc.p3.gr1.grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1Application.class, args);
+	}
+
+}
