e4a517ed8240e47ea28f189dca9736fc764a54cc
lautimorales <lautimorales2002@gmail.com>
2024-04-25 20:50:35 -0300

    Creacion de entidad PaymentMethod

--- src/main/java/com/imb4/gc/p3/gr1/entity/PaymentMethod.java
+++ src/main/java/com/imb4/gc/p3/gr1/entity/PaymentMethod.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;		//Preguntar, ¿Qué anotación importar?
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "payment_method")
