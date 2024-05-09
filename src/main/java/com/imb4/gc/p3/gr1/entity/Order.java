c8d2fe2a6e7f37873a3a36c331c954770e9eef69
Eduardo Manucha <edumanucha@gmail.com>
2024-05-08 22:03:26 -0300

    Creación entidad Order

---
+++ src/main/java/com/imb4/gc/p3/gr1/entity/Order.java
@@ -0,0 +1,78 @@
+package com.imb4.gc.p3.gr1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id_order;
+    private String user;
+    private String cart;
+    private String date;
+    private String state;
+    private String address;
+    private double total;
+
+    public Order() {}
+
+    public Long getId_order() {
+        return id_order;
+    }
+
+    public void setId_order(Long id_order) {
+        this.id_order = id_order;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getCart() {
+        return cart;
+    }
+
+    public void setCart(String cart) {
+        this.cart = cart;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
