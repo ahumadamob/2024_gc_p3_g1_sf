7cb0bb479b4bddd2a2f1487d001bbf2cc9d8db83
oller.esteban <estebanoller73@gmail.com>
2024-05-16 16:32:41 -0300

    Creacion entidad CartProduct, permite relacionar las entidaddes de Cart y Product

---
+++ src/main/java/com/imb4/gc/p3/gr1/entity/CartProduct.java
@@ -0,0 +1,49 @@
+package com.imb4.gc.p3.gr1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class CartProduct {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public CartProduct() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
