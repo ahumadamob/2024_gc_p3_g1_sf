1cf78ac39347cca1160d2b4acf4793ded0d75614
Lmonzon1305 <aronmonzon@gmail.com>
2024-05-16 18:04:57 -0300

    Creación entidad ProductCategory.java

---
+++ src/main/java/com/imb4/gc/p3/gr1/entity/ProductCategory.java
@@ -0,0 +1,48 @@
+package com.imb4.gc.p3.gr1.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class ProductCategory {
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+	public ProductCategory() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+    
+}
