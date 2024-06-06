863db23b0ffc4a8f8f3f0a850e6db0b8c2c13acd
Mario Ahumada <ahumadamob@gmail.com>
2024-06-06 16:33:51 -0300

    Correcciones del profesor

--- src/main/java/com/imb4/gc/p3/gr1/entity/Category.java
+++ src/main/java/com/imb4/gc/p3/gr1/entity/Category.java
@@ -3,12 +3,10 @@ package com.imb4.gc.p3.gr1.entity;
 import java.sql.Date;
 import java.util.List;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.ManyToMany;
 
 @Entity
