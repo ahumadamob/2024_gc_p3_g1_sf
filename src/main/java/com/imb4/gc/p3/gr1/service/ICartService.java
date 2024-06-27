0992640cd6449d70e77a9d44494c2d9dbdfd4e85
esteban <estebanoller73@gmail.com>
2024-06-27 17:30:11 -0300

    iCartService

---
+++ src/main/java/com/imb4/gc/p3/gr1/service/ICartService.java
@@ -0,0 +1,12 @@
+package com.imb4.gc.p3.gr1.service;
+
+import java.util.List;
+import com.imb4.gc.p3.gr1.entity.Cart;
+
+public interface ICartService {
+    List<Cart> getAll();
+    Cart getById(Long id);
+    Cart save(Cart cart);
+    void delete(Long id);
+    boolean exists(Long id);
+}
