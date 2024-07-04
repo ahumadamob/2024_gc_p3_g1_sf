ba80e60441eead43aae7d5bb426a1f27f6312241
BNFLYD <moralesflaviog@gmail.com>
2024-07-04 20:43:31 -0300

    Recuperatorio Falvio Morales

--- src/main/java/com/imb4/gc/p3/gr1/repository/PaymentMethodRepository.java
+++ src/main/java/com/imb4/gc/p3/gr1/repository/PaymentMethodRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.imb4.gc.p3.gr1.entity.PaymentMethod;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long>{
-
+	
 }
